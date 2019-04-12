7aedc1cf21241438866f901629361dd7b9c0e839
odifek <kennedy998@gmail.com>
2019-04-12 12:23:29 +0100

    Use Material App

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,50 @@
 import 'package:flutter/material.dart';
 
-class MyAppBar extends StatelessWidget {
-  MyAppBar({this.title});
-
-  final Widget title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 56.0,
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: BoxDecoration(color: Colors.purple[500]),
-
-      // Row is a horizontal, linear layout
-      child: Row(
-        children: <Widget>[
-          IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: null,
-            tooltip: 'Navigation Menu',
-          ),
-          // Expanded expands its child to fill the available space
-          Expanded(child: title),
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: null,
-            tooltip: 'Search',
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class MyScaffold extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      // Column is vertical linear layout
-      child: Column(
-        children: <Widget>[
-          MyAppBar(
-            title: Text(
-              'Lovely Appbar',
-              style: Theme.of(context).primaryTextTheme.title,
-            ),
-          ),
-          Expanded(
-              child: Center(
-            child: Text("Hello, world!"),
-          ))
-        ],
-      ),
-    );
-  }
-}
-
 void main() {
   runApp(MaterialApp(
-    title: "My App",
-    home: MyScaffold(),
+    title: "Flutter Tutorial",
+    home: TutorialHome(),
+    theme: ThemeData(
+      primaryColor: Colors.indigo[500],
+      primaryColorDark: Colors.indigo[700],
+      accentColor: Colors.amberAccent,
+    ),
   ));
+}
+
+class TutorialHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: null,
+          tooltip: "Navigation Menu",
+        ),
+        title: Text('Awesome Title'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          )
+        ],
+      ),
+      body: Center(
+        child: Text('Hello worlds!'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Add',
+        child: Icon(
+          Icons.add,
+        ),
+        onPressed: doNothing,
+      ),
+    );
+  }
+
+  void doNothing() {
+    print("You mean nothing?");
+  }
 }
